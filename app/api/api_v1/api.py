e045419d3d7b63b0eca6fef8d07887e0db396110
queprocno <queprocno@example.com>
2021-10-21 02:34:49 +0200

    add get-record data endpoint

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import items, login, users, utils, topics
+from app.api.api_v1.endpoints import items, login, data, users, utils, topics
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(topics.router, prefix="/topics", tags=["topics"])
+api_router.include_router(data.router, prefix="/data", tags=["data"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
