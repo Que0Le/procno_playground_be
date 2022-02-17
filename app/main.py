84d74043c077fc81e9a7e370bcef144a4b2fba6d
queprocno <lebanhat95@gmail.com>
2022-02-17 15:25:54 +0100

    fix cors temp

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app.mount("/static", StaticFiles(directory="./data/static"), name="static")
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        # allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
