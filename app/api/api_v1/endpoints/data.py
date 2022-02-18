4e048dd07cac7396b12308653ed39bf8a504273c
queprocno <lebanhat95@gmail.com>
2022-02-18 21:14:05 +0100

    .

--- app/api/api_v1/endpoints/data.py
+++ app/api/api_v1/endpoints/data.py
@@ -1,21 +1,25 @@
-from fastapi.responses import StreamingResponse
+# from fastapi.responses import StreamingResponse
 from fastapi.responses import FileResponse
-from fastapi import APIRouter, Body, Depends
-import io
+from fastapi import APIRouter
+# import io
 import random
-from app.utilities.strings import slugify, clean_filename
+import os
+from starlette.responses import StreamingResponse
+
+from app.utilities.strings import clean_filename
+
 router = APIRouter()
 
 
-def iterfile(path: str):
+def iter_file(path: str):
     with open(path, mode="rb") as file_like:
         yield from file_like
 
 
 @router.get("/streaming-response/")
-async def get_streaming_response() -> io.BytesIO:
+async def get_streaming_response() -> StreamingResponse:
     return StreamingResponse(
-        iterfile("./data/records/imperial_march_60.wav"),
+        iter_file("./data/records/imperial_march_60.wav"),
         media_type="audio/wave"
     )
 
@@ -28,15 +32,17 @@ async def get_file_response():
 @router.get("/records/{record_filename}")
 async def get_record_by_file(record_filename: str):
     return FileResponse(
-        #TODO: Implement clean_filename. Its still a placeholder function
+        # TODO: Implement clean_filename. Its still a placeholder function
         "./data/records/" + clean_filename(record_filename)
     )
 
-@router.get("/dummies-records/{record_filename}")
-async def get_dummies_record_by_random(record_filename: str):
-    """ Don't care about the record_filename. This endpoint returns
-    one of 3 audio files randomly """
-    files = ['imperial_march_60.wav', 'star_wars_3.wav', 'star_wars_60.wav']
+
+@router.get("/dummies-records/")
+async def get_dummies_record_by_random():
+    """ Return a random audio file ("mp3", "wav") in records folder """
+    listdir = os.listdir("./data/records/")
+    listfiles = [item for item in listdir if item.split(".")[-1] in ["mp3", "wav"]]
+    random_file = random.choice(listfiles)
     return FileResponse(
-        "./data/records/" + random.choice(files)
+        "./data/records/" + random_file
     )
